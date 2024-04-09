35e104ddf6862948f87e838bd64c2f08ef8c0cd0
Sahil <sahillukose@gmail.com>
2024-04-09 10:59:23 +0100

    Add project files.

---
+++ test-1/Controllers/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using test_1.Models;
+
+namespace test_1.Controllers
+{
+    public class ItemController : Controller
+    {
+        private List<Item> _items = new List<Item>(); // Simulated database
+
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View(_items);
+        }
+
+        // GET: Item/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Item/Create
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            _items.Add(item); // Add item to the list (simulated database)
+            return RedirectToAction("Index");
+        }
+    }
+
+}
